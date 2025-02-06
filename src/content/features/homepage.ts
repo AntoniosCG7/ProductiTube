1d33a123932b1c163141a79851d09fc00143442e
Antonios Chatzinikolas <antonis_132000@hotmail.com>
2025-02-06 15:30:28 -0500

    Remove homepage feature implementation

--- src/content/features/homepage.ts
+++
@@ -1,8 +0,0 @@
-export const initializeHomepage = () => {
-  if (window.location.pathname === '/') {
-    const homepage = document.querySelector('ytd-rich-grid-renderer');
-    if (homepage) {
-      (homepage as HTMLElement).style.display = 'none';
-    }
-  }
-}; 
