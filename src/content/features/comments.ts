d42c3935e8016871bb1bcf92137f568572b73ddb
Antonios Chatzinikolas <antonis_132000@hotmail.com>
2025-05-31 11:56:50 -0400

    Remove standalone feature files that previously contained uncategorized logic

--- src/content/features/comments.ts
+++
@@ -1,6 +0,0 @@
-export const initializeComments = (enable: boolean) => {
-  const commentsSection = document.querySelector('ytd-comments');
-  if (commentsSection) {
-    (commentsSection as HTMLElement).style.display = enable ? 'none' : '';
-  }
-};
