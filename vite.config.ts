e3561ace4ffc5245e7af3a288e5217fde699534b
Antonios Chatzinikolas <antonis_132000@hotmail.com>
2025-06-15 23:19:01 -0400

    Integrate Tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import { copyManifest } from './build-utils/copy-manifest-plugin';
 import { buildConfig } from './build-utils/build-config';
 import { paths } from './build-utils/paths';
 
 export default defineConfig({
-  plugins: [react(), copyManifest()],
+  plugins: [react(), copyManifest(), tailwindcss()],
   resolve: {
     alias: [{ find: '@', replacement: paths.srcDir }],
   },
