a21a73609b92fc8d12fbcfbd80ec96b8cf8360a8
Antonios Chatzinikolas <antonis_132000@hotmail.com>
2025-06-17 20:55:10 -0400

    Update Header component to adjust padding

--- src/popup/components/Header/Header.tsx
+++ src/popup/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Header: React.FC = () => (
-  <div className="bg-[#f52f30] py-4 text-center text-white">
+  <div className="bg-[#f52f30] py-2 text-center text-white">
     <h1 className="text-2xl font-bold tracking-tight">ProductiTube</h1>
     <p className="text-lg opacity-80">Your YouTube, Your Rules</p>
   </div>
